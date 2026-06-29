ee0e819dc93281ce656103d11f4288ee7e5670a7
GDK-13 <kevingdkt@gmail.com>
2026-06-29 08:45:14 -0300

    Update Hero component description for clarity and conciseness

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -9,11 +9,10 @@ export default function Hero() {
           <div>
             <h2 className="mb-6">FUNPAPI</h2>
             <p className="text-foreground leading-relaxed mb-4">
-              A Fundação de proteção ao meio Ambiente e Ecoturismo do Estado do Piauí – Funpapi foi constituída em 2002 com atuação há 18 anos. É uma entidade sem fins lucrativos, de direito privado, reconhecida pelo Município de Teresina como de Utilidade Pública através da Lei nº 3579 de 20/11/2006. A Fundação é formada por sócio-fundadores, colaboradores e parceiros, constituindo equipes multiprofissionais que atuam nas áreas de agronomia, jornalismo, administração, direito, biologia, economia, assistência social e pedagogia dentro de uma perspectiva interdisciplinar, visando a complementariedade do saber.
+              A Fundação de proteção ao meio Ambiente e Ecoturismo do Estado do Piauí – Funpapi foi constituída em 2002 com atuação há 18 anos. É uma entidade sem fins lucrativos, de direito privado, reconhecida pelo Município de Teresina como de Utilidade Pública através da Lei nº 3579 de 20/11/2006. A Fundação é formada por sócio-fundadores, colaboradores e parceiros, constituindo equipes multiprofissionais que atuam nas áreas de agronomia, jornalismo, administração, direito, biologia, economia, assistência social e pedagogia dentro de uma perspectiva interdisciplinar, visando a complementariedade do saber.A Fundação de Proteção ao Meio Ambiente e Ecoturismo do Estado do Piauí - FUNPAPI foi constituída em 2002, é uma entidade filantrópica, reconhecida pelo Município de Teresina como de Utilidade Pública através da Lei nº 3579 de 20/11/2006 e pelo estado do Piauí através da Lei nº 7.756 de 21/03/2022.
             </p>
             <p className="text-foreground leading-relaxed">
-              A ONG atua em diversas frentes de trabalho, ciente de que essa atuação exige uma perspectiva interdisciplinar para garantir um futuro melhor a quem necessita.
-            </p>
+              A Fundação é formada por sócios fundadores, colaboradores e parceiros, constituindo equipes multiprofissionais que atuam nas áreas de agronomia, jornalismo, administração, direito, biologia, economia, assistência social e pedagogia dentro de uma perspectiva interdisciplinar, visando a complementariedade do saber.
           </div>
           <div className="rounded-2xl overflow-hidden shadow-lg">
             <ImageWithFallback
